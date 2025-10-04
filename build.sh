21b4ffc79f03bbb2fed2e222e6e201137789f162
Jonathan <jonathanbuko@icloud.com>
2025-10-04 18:40:02 +0200

    build in parallel

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 export CGO_ENABLED=0
 export GOOS=linux
 
-# build for amd64, arm64 and armv7
-GOARCH=amd64 go build -o build/main-amd64 ./cmd/main.go
-GOARCH=arm64 go build -o build/main-arm64 ./cmd/main.go
-GOARCH=arm GOARM=7 go build -o build/main-arm ./cmd/main.go
+go mod download
+
+GOARCH=amd64 go build -o build/main-amd64 ./cmd/main.go &
+GOARCH=arm64 go build -o build/main-arm64 ./cmd/main.go &
+GOARCH=arm GOARM=7 go build -o build/main-arm ./cmd/main.go &
+
+wait
