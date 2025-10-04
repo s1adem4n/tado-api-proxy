44376dc6670a710a7b5be0b6ecac0fa2c8c235d5
Jonathan <jonathanbuko@icloud.com>
2025-10-04 18:35:49 +0200

    fix build naming

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ export GOOS=linux
 # build for amd64, arm64 and armv7
 GOARCH=amd64 go build -o build/main-amd64 ./cmd/main.go
 GOARCH=arm64 go build -o build/main-arm64 ./cmd/main.go
-GOARCH=arm GOARM=7 go build -o build/main-armv7 ./cmd/main.go
+GOARCH=arm GOARM=7 go build -o build/main-arm ./cmd/main.go
