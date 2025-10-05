abd0d266dd9332fd9530559c9de10d7cda361379
Jonathan <jonathanbuko@icloud.com>
2025-10-05 13:43:36 +0200

    remove linux/arm/v7 target

--- build.sh
+++ build.sh
@@ -7,6 +7,5 @@ go mod download
 
 GOARCH=amd64 go build -o build/main-amd64 ./cmd/main.go &
 GOARCH=arm64 go build -o build/main-arm64 ./cmd/main.go &
-GOARCH=arm GOARM=7 go build -o build/main-arm ./cmd/main.go &
 
 wait
