93a3ee7a34d2cc5772e289565ad9ab68f0a7cbc6
Jonathan <jonathanbuko@icloud.com>
2025-11-17 16:19:38 +0100

    exit on error in build script

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -e  # Exit on error
+
 export CGO_ENABLED=0
 export GOOS=linux
 
 go mod download
 
 GOARCH=amd64 go build -o build/main-amd64 ./cmd/main.go &
+pid1=$!
 GOARCH=arm64 go build -o build/main-arm64 ./cmd/main.go &
+pid2=$!
 
-wait
+# Wait for all background jobs and capture their exit codes
+wait $pid1 || exit 1
+wait $pid2 || exit 1
