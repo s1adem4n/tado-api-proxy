501c3dab4273d3d6ac6e1f212eb22f2eeeaa91cd
Jonathan <jonathanbuko@icloud.com>
2025-11-17 16:08:41 +0100

    fix: only append url query if in original request

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,6 +1,8 @@
 package proxy
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"maps"
 	"net/http"
@@ -29,11 +31,18 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	url := BaseURL + r.URL.Path
+	if r.URL.RawQuery != "" {
+		url += "?" + r.URL.RawQuery
+	}
+
+	bodyCopy, _ := io.ReadAll(r.Body)
+
 	req, err := http.NewRequestWithContext(
 		r.Context(),
 		r.Method,
-		BaseURL+r.URL.Path+"?"+r.URL.RawQuery,
-		r.Body,
+		url,
+		bytes.NewReader(bodyCopy),
 	)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -48,6 +57,27 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
+
+	respBodyCopy, _ := io.ReadAll(resp.Body)
+	resp.Body = io.NopCloser(bytes.NewReader(respBodyCopy))
+
+	fmt.Printf("----REQUEST %s----\n", req.URL.Path)
+	for k, v := range req.Header {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	fmt.Println()
+	fmt.Printf("----RESPONSE %s----\n", req.URL.Path)
+	for k, v := range resp.Header {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+	fmt.Printf("Status Code: %d\n", resp.StatusCode)
+	fmt.Println("Body:")
+	fmt.Println(string(respBodyCopy))
+	fmt.Println("Request Body:")
+	fmt.Println(string(bodyCopy))
+	fmt.Println("---------------")
+	fmt.Println()
 
 	maps.Copy(w.Header(), resp.Header)
 	w.WriteHeader(resp.StatusCode)
